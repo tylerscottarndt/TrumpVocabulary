78dd01229f6fa9019ee9234966a3246a0f7f520e
Tyler Arndt <tsarndt16@gmail.com>
2020-04-11 15:42:35 -0500

    read urls from txt file

--- src/Main.py
+++ src/Main.py
@@ -9,31 +9,13 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
 if __name__ == '__main__':
-    urls = [
-            "https://www.rev.com/blog/transcripts/donald-trump-charlotte-north-carolina-rally-transcript-trump-holds-rally-before-super-tuesday",
-            "https://www.rev.com/blog/transcripts/donald-trump-charleston-south-carolina-rally-transcript-february-28-2020",
-            "https://www.rev.com/blog/transcripts/donald-trump-las-vegas-nevada-rally-transcript",
-            "https://www.rev.com/blog/transcripts/donald-trump-colorado-springs-co-rally-transcript",
-            "https://www.rev.com/blog/transcripts/donald-trump-phoenix-arizona-rally-transcript",
-            "https://www.rev.com/blog/transcripts/donald-trump-new-hampshire-rally-february-10-2020",
-            "https://www.rev.com/blog/transcripts/donal-trump-iowa-rally-transcript-trump-holds-rally-in-des-moines-iowa",
-            "https://www.rev.com/blog/transcripts/donald-trump-new-jersey-rally-speech-transcript-trump-holds-rally-in-wildwood-nj",
-            "https://www.rev.com/blog/transcripts/donald-trump-milwaukee-rally-transcript-trump-holds-rally-during-iowa-democratic-debate",
-            "https://www.rev.com/blog/transcripts/donald-trump-michigan-rally-transcript-trump-holds-a-rally-in-battle-creek-during-impeachment",
-            "https://www.rev.com/blog/transcripts/donald-trump-hershey-pennsylvania-rally-transcript-december-10-2019",
-            "https://www.rev.com/blog/transcripts/donald-trump-kentucky-rally-speech-transcript-lexington-kentucky-rally",
-            "https://www.rev.com/blog/transcripts/donald-trump-mississippi-rally-speech-transcript-2019-rally-in-tupelo-mississippi",
-            "https://www.rev.com/blog/transcripts/donald-trump-dallas-rally-speech-transcript-october-17-2019",
-            "https://www.rev.com/blog/transcripts/donald-trump-minnesota-rally-speech-transcript-minneapolis-mn-rally-october-10-2019",
-            "https://www.rev.com/blog/transcripts/donald-trump-narendra-modi-houston-tx-rally-transcript-trump-and-modi-hold-texas-rally",
-            "https://www.rev.com/blog/transcripts/donald-trump-new-mexico-rally-transcript-full-speech-transcript",
-            "https://www.rev.com/blog/transcripts/donald-trump-north-carolina-rally-transcript-in-fayetteville-nc-september-9-2019",
-            "https://www.rev.com/blog/transcripts/donald-trump-new-hampshire-rally-transcript-august-15-2019",
-            "https://www.rev.com/blog/transcripts/donald-trump-ohio-rally-speech-transcript-full-transcript-of-august-1-2019-rally-in-cincinnati",
-            "https://www.rev.com/blog/transcripts/donald-trump-maga-event-speech-transcript-north-carolina-rally"
-            ]
+    rally_urls = []
+    rally_url_file = open('rally_urls.txt')
+    for line in rally_url_file:
+        rally_urls.append(line.replace('\n', ''))
+    rally_url_file.close()
 
-    speech_loc = ['Charlotte_NC', 'Charleston_SC', 'Las_Vegas_NV', 'Colorado_Springs_CO', 'Phoenix_AZ',
+    rally_location = ['Charlotte_NC', 'Charleston_SC', 'Las_Vegas_NV', 'Colorado_Springs_CO', 'Phoenix_AZ',
                   'Manchester_NH_01', 'Des_Moines, IA', 'Wildwood_NJ', 'Milwaukee_WS', 'Battle_Cree_MI',
                   'Hershey_PA', 'Lexington_KY', 'Tupelo_MS', 'Dallas_TX', 'Minneapolis_MN', 'Houston_TX',
                   'Albuquerque_NM', 'Fayetteville_NC', 'Manchester_NH_02', 'Cincinnati_OH', 'Greenville_NC']
@@ -45,13 +27,13 @@ if __name__ == '__main__':
     except:
         speeches = []
         print("Loading and Cleaning URLs...")
-        for url in urls:
+        for url in rally_urls:
             transcript = DataFormatter.url_to_transcript(url)
             clean_transcript = DataFormatter.clean_transcript(transcript)
             speeches.append(clean_transcript)
 
         print("Creating Dictionary of {key: location, value:Speech}...")
-        speech_dict = dict(zip(speech_loc, speeches))
+        speech_dict = dict(zip(rally_location, speeches))
         speech_df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in speech_dict.items()]))
 
         print("Saving DataFrame...")
